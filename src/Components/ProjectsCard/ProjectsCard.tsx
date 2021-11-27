f5cf63d7707ac60673a455e7caa80daac1b0f0a3
Harry Sanders <harry.sandersiv@yahoo.com>
2021-11-26 21:35:30 -0500

    updating projects

--- src/Components/ProjectsCard/ProjectsCard.tsx
+++ src/Components/ProjectsCard/ProjectsCard.tsx
@@ -2,9 +2,10 @@ import styles from './ProjectsCard.module.css';
 
 const projects = [
    {id: 0, name: `Expenses-Tracker`, link: `https://harrysiv.github.io/expenses-chart/`, description: `A small application for users to input and track their expenses year by year.`},
-   {id: 1, name: `Jokes`, link: `none`, description: `An app that fetches a random joke and displays it.`},
-   {id: 2, name: `D&D`, link: `none`, description: `An app that creates a character and allows the user to fight an enemy text-rpg style.`},
-   {id: 3, name: `Star Wars Ships`, link: `https://harrysiv.github.io/swships/`, description: `An app that fetches Star Wars Ship info from an API and displays it.`},
+   {id: 1, name: `Star Wars Ships`, link: `https://harrysiv.github.io/swships/`, description: `An app that fetches Star Wars ship info and displays it.`},
+   {id: 2, name: `Students`, link: `https://harrysiv.github.io/fetchstudents-app/`, description: `An app that fetches a list of students and displays them.`},
+   {id: 3, name: `Jokes`, link: `https://harrysiv.github.io/jokes/`, description: `An app that fetches a random joke and displays it.`},
+   {id: 4, name: `D&D`, link: `none`, description: `An app that creates a character and allows the user to fight an enemy, text-rpg style.`},
 ];
 
 
@@ -18,8 +19,9 @@ const ProjectsCard = () => {
          {projects.map(project => (
             <a className={styles.card} href={project.link}>
                <div className={styles.topCard}>
-                  <h2>{project.name}</h2>
+                  <h2 className={styles.projName}>{project.name}</h2>
                </div>
+               <hr />
                <h6 id={`${project.id}`} >{project.description}</h6>
             </a>
          ))}
