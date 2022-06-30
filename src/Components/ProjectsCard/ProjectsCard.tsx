7c9d74c480eacc6065d22a75793e86e0670e79c7
Harry Sanders <harry.sandersiv@yahoo.com>
2022-06-29 23:12:10 -0400

    updated dependencies to their latest versions

--- src/Components/ProjectsCard/ProjectsCard.tsx
+++ src/Components/ProjectsCard/ProjectsCard.tsx
@@ -3,8 +3,8 @@ import styles from './ProjectsCard.module.css';
 const projects = [
    {id: 0, name: `Restaurant Website`, link: `https://not-a-restaurant.firebaseapp.com/`, description: `A simple SPA website for a fake restaurant, hosted on Firebase.`},
    {id: 1, name: `Expenses-Tracker`, link: `https://harrysiv.github.io/expenses-chart/`, description: `A small application for users to input and track their expenses year by year.`},
-   {id: 2, name: `Star Wars Ships`, link: `https://harrysiv.github.io/swships/`, description: `An app that fetches Star Wars ship info and displays it.`},
-   {id: 3, name: `Students`, link: `https://harrysiv.github.io/fetchstudents-app/`, description: `An app that fetches a list of students and displays them.`},
+   {id: 2, name: `Star Wars Ships`, link: `https://harrysiv.github.io/swships/`, description: `An application that fetches Star Wars ship info and displays it.`},
+   {id: 3, name: `Students`, link: `https://harrysiv.github.io/fetchstudents-app/`, description: `An application that fetches a list of students and displays them.`},
    {id: 4, name: `Jokes`, link: `https://harrysiv.github.io/jokes/`, description: `An app that fetches a random joke and displays it.`},
    {id: 5, name: `D&D`, link: `https://harrysiv.github.io/Dndjs/`, description: `An app that creates a character and allows the user to fight an enemy, text-rpg style.`},
 ];
