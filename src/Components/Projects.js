762613aeea30738664320ae976c3a819ac07ec22
Harry Sanders <harry.sandersiv@yahoo.com>
2022-06-30 22:53:53 -0400

    had to change projects-card to projectscard

---
+++ src/Components/Projects.js
@@ -0,0 +1,18 @@
+import ProjectsCard from './projectscard/ProjectsCard';
+import styles from '../App.module.css';
+
+
+const Projects = () => {
+   return (
+      <section className={styles.altcontainer}>
+         <h2 className={styles.pageTitle}>Example Projects</h2>
+         <ProjectsCard />
+         <div className={styles.langPadding}>
+         <h2 className={styles.projLang}>Languages: JavaScript, HTML, CSS, Typescript</h2>
+         <h2 className={styles.projLang}>Libraries/Frameworks: React w/ Hooks & Routing, Next, Axios, Node, Express</h2>
+         </div>
+      </section>
+   )
+}
+
+export default Projects;
