8fbdb73ef8316e84a99e099c46f0cb70e0d65b00
Harry Sanders <harry.sandersiv@yahoo.com>
2022-07-01 01:13:00 -0400

    update resume to include tools used on each project card

--- src/Components/ProjectsCard/ProjectsCard.js
+++ src/Components/ProjectsCard/ProjectsCard.js
@@ -1,12 +1,12 @@
 import styles from './ProjectsCard.module.css';
 
 const projects = [
-   {id: 0, name: `Restaurant Website`, link: `https://not-a-restaurant.firebaseapp.com/`, description: `A simple SPA website for a fake restaurant, hosted on Firebase.`},
-   {id: 1, name: `Expenses-Tracker`, link: `https://harrysiv.github.io/expenses-chart/`, description: `A small application for users to input and track their expenses year by year.`},
-   {id: 2, name: `Star Wars Ships`, link: `https://harrysiv.github.io/swships/`, description: `An application that fetches Star Wars ship info and displays it.`},
-   {id: 3, name: `Students`, link: `https://harrysiv.github.io/fetchstudents-app/`, description: `An application that fetches a list of students and displays them.`},
-   {id: 4, name: `Jokes`, link: `https://harrysiv.github.io/jokes/`, description: `An app that fetches a random joke and displays it.`},
-   {id: 5, name: `D&D`, link: `https://harrysiv.github.io/Dndjs/`, description: `An app that creates a character and allows the user to fight an enemy, text-rpg style.`},
+   {id: 0, name: `Restaurant Website`, tools: 'React/Routing, Firebase', link: `https://not-a-restaurant.firebaseapp.com/`, description: `A simple SPA website for a fake restaurant, hosted on Firebase.`},
+   {id: 1, name: `Expenses-Tracker`, tools: 'React',link: `https://harrysiv.github.io/expenses-chart/`, description: `A small application for users to input and track their expenses year by year.`},
+   {id: 2, name: `Star Wars Ships`, tools: 'React, Axios',link: `https://harrysiv.github.io/swships/`, description: `An application that fetches Star Wars ship info and displays it.`},
+   {id: 3, name: `Students`, tools: 'React, Axios',link: `https://harrysiv.github.io/fetchstudents-app/`, description: `An application that fetches a list of students and displays them.`},
+   {id: 4, name: `Jokes`, tools: 'Javascript, Axios',link: `https://harrysiv.github.io/jokes/`, description: `An app that fetches a random joke and displays it.`},
+   {id: 5, name: `D&D`, tools: 'Javascript',link: `https://harrysiv.github.io/Dndjs/`, description: `An app that creates a character and allows the user to fight an enemy, text-rpg style.`},
 ];
 
 
@@ -24,6 +24,7 @@ const ProjectsCard = () => {
                </div>
                <hr />
                <h6 id={`${project.id}`} >{project.description}</h6>
+               <h5>{project.tools}</h5>
             </a>
          ))}
       </div>
